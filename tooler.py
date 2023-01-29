a705173076671ba15c9391b4c9eb9583522da41f
UcKAHD3P <123495394+UcKAHD3P@users.noreply.github.com>
2023-01-29 15:06:49 +0300

    Update tooler.py

--- tooler.py
+++ tooler.py
@@ -2,23 +2,26 @@ import os
 from tkinter import *
 import time
 from tkinter import messagebox
-def self():
+import threading
+def selfbot():
     print('Запускаю селф-бота...')
-    time.sleep(3)
     os.system('python ./tools/selfbot/main.py')
+def start_selfbot():
+    threading.Thread(target=self).start()
 def crashbot():
     print('Запускаю краш-бота...')
-    time.sleep(3)
     os.system('python ./tools/crashbot.py')
+def start_crashbot():
+    threading.Thread(target=crashbot).start()
 def about():
     messagebox.showinfo(title='Tooler', message='Discord Tools by internetmilitarist#5585')
 root=Tk()
 root.title('TOOLER BY INTERNETMILITARIST')
 root.geometry('550x320')
 root.configure(background='grey')
-selfbot = Button(text='Селф-бот',background='red', command=self, width=10)
+selfbot = Button(text='Селф-бот',background='red', command=start_selfbot, width=10)
 selfbot.place(x=10, y=10)
-crashbot = Button(text='Краш-бот', background='red', command=crashbot, width=10)
+crashbot = Button(text='Краш-бот', background='red', command=start_crashbot, width=10)
 crashbot.place(x=10, y=45)
 about = Button(text='About tooler', background='grey', command=about, width=20)
 about.place(x=150, y=45)
