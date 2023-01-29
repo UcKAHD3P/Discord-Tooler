e9c6a3cb6ef55fce9f91967ffdf949da1fb6b1bc
UcKAHD3P <123495394+UcKAHD3P@users.noreply.github.com>
2023-01-29 15:25:16 +0300

    Update tooler.py

--- tooler.py
+++ tooler.py
@@ -13,6 +13,11 @@ def crashbot():
     os.system('python ./tools/crashbot.py')
 def start_crashbot():
     threading.Thread(target=crashbot).start()
+def status():
+    print('Запускаю DiscordRPC...')
+    os.system('python ./tools/discordrpc/status.py')
+def start_status():
+    threading.Thread(target=status).start()
 def about():
     messagebox.showinfo(title='Tooler', message='Discord Tools by internetmilitarist#5585')
 root=Tk()
@@ -23,6 +28,8 @@ selfbot = Button(text='Селф-бот',background='red', command=start_selfbot,
 selfbot.place(x=10, y=10)
 crashbot = Button(text='Краш-бот', background='red', command=start_crashbot, width=10)
 crashbot.place(x=10, y=45)
+status = Button(text='DiscordRPC', background='red', command=start_status, width=10)
+status.place(x=10, y=85
 about = Button(text='About tooler', background='grey', command=about, width=20)
 about.place(x=150, y=45)
 root.mainloop()
